cc35c4d6ee0b62d99a9f6a403fb019209722d01b
Nils Leuzinger <18655912+PawkyPenguin@users.noreply.github.com>
2022-10-08 12:58:10 +0200

    Replace common skelly spawns with my skellys. Maybe I have too many?
    There might be additional spawn checks I'm circumventing, because if I
    deny spawn events of all mobs and just respawn that mob, I have a ton of
    animals. Maybe some additional checks should apply before spawning that
    mob in the level.

--- src/main/java/com/example/hardai/CommonMCEvents.java
+++ src/main/java/com/example/hardai/CommonMCEvents.java
@@ -1,17 +1,36 @@
 package com.example.hardai;
 
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.MobSpawnType;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
 import net.minecraftforge.event.entity.living.LivingSpawnEvent;
 import net.minecraftforge.eventbus.api.Event;
+import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
 @Mod.EventBusSubscriber(modid = ExampleMod.MODID, bus = Mod.EventBusSubscriber.Bus.FORGE)
 public class CommonMCEvents {
+    private static final IEventBus mcBus = Mod.EventBusSubscriber.Bus.FORGE.bus().get();
 
     @SubscribeEvent
     public static void checkSpawn (LivingSpawnEvent.CheckSpawn event) {
-        event.getEntity().remove(Entity.RemovalReason.DISCARDED);
-        event.setResult(Event.Result.DENY);
+        var mob = event.getEntity();
+        var type = mob.getType();
+        if(type == EntityType.SKELETON) {
+            mob.remove(Entity.RemovalReason.DISCARDED);
+            event.setResult(Event.Result.DENY);
+            Player player = null;
+            ItemStack item = null;
+            boolean expandAABBToMinusY = false;
+            boolean spawnWithUpwardOffset = true;
+            ServerLevel level = mob.getServer().getLevel(mob.level.dimension());
+            ExampleMod.MY_SKELETON.get().spawn(level, item, player, event.getEntity().blockPosition(), MobSpawnType.NATURAL, spawnWithUpwardOffset, expandAABBToMinusY);
+            EntityType.AXOLOTL.spawn(level, item, player, event.getEntity().blockPosition(), MobSpawnType.NATURAL, spawnWithUpwardOffset, expandAABBToMinusY);
+        }
     }
 }
